ce8f11fc4ab07a05d1a14501dcfe44f04be5062c
moq4 <ashe5695@gmail.com>
2025-03-25 01:51:15 +1100

    Matrix add and subtract methods

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@ mod matrix;
 use crate::matrix::matrix::Matrix;
 
 fn main() {
-    let m = Matrix::new(3, 3);
-    m.print();
+    let mut m1 = Matrix::new(2, 2);
+    m1.data = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
+
+    let mut m2 = Matrix::new(2, 2);
+    m2.data = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
+
+    let add_result = m1.add(&m2);
+    let sub_result = m1.subtract(&m2);
+
+    println!("Addition results");
+    add_result.print();
+
+    println!("Subtraction results");
+    sub_result.print();
 }
--- src/matrix/matrix.rs
+++ src/matrix/matrix.rs
@@ -1,7 +1,7 @@
 pub struct Matrix {
     pub rows: usize,
     pub cols: usize,
-    pub data: Vec<Vec<f64>>
+    pub data: Vec<Vec<f64>>,
 }
 
 impl Matrix {
@@ -17,5 +17,37 @@ impl Matrix {
         for row in &self.data {
             println!("{:?}", row);
         }
+    }
+
+    pub fn add(&self, other: &Matrix) -> Matrix {
+        assert!(
+            self.rows == other.rows && self.cols == other.cols,
+            "Matrix dimensions must match for addition"
+        );
+
+        let mut result = Matrix::new(self.rows, self.cols);
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                result.data[i][j] = self.data[i][j] + other.data[i][j];
+            }
+        }
+
+        result
+    }
+
+    pub fn subtract(&self, other: &Matrix) -> Matrix {
+        assert!(
+            self.rows == other.rows && self.cols == other.cols,
+            "Matrix dimensions must match for subtraction"
+        );
+
+        let mut result = Matrix::new(self.rows, self.cols);
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                result.data[i][j] = self.data[i][j] - other.data[i][j];
+            }
+        }
+
+        result
     }
 }
